5bec1ed225c0ff8e87d29285e9b97850fe539647
Vitaliy Filippov <vitalif@yourcmc.ru>
2018-01-22 19:28:55 +0300

    Build script

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 git clone https://github.com/mediawiki4intranet/configs
 cd configs && git show --quiet --format='%cd-%H' --date=short HEAD > ../mediawiki4intranet-version && cd .. && rm -rf configs
-docker build -t mediawiki4intranet .
+docker build -t mediawiki4intranet . && \
 docker build -t mediawiki4intranet/ve -f ve.Dockerfile .
