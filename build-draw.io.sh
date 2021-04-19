22d780c3cb81de4081cecdae40f0e630db8d146c
stas <stas-fomin@yandex.ru>
2021-04-19 13:02:58 +0300

    Adapt to building drawio.war to DNF-YUM system, also fix building war

--- build-draw.io.sh
+++ build-draw.io.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 # A script to build a self-hosted draw.io instance
+YUM_CMD=$(which yum)
+DNF_CMD=$(which dnf)
+APT_GET_CMD=$(which apt-get)
 
 set -e
 
-sudo apt-get install default-jdk ant
+ if [[ ! -z $APT_GET_CMD ]]; then
+    sudo apt-get install default-jdk ant
+ elif [[ ! -z $DNF_CMD ]]; then
+    sudo dnf install ant
+ elif [[ ! -z $YUM_CMD ]]; then
+    sudo yum install ant
+ else
+    echo "Please, install Ant/JDK"
+ fi
+
 [ -e drawio ] || git clone https://github.com/mediawiki4intranet/drawio
 cd drawio/etc/build
-ant
+ant war
 cd ../../..
 cp drawio/build/draw.war ./
